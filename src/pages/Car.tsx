8c19dc69c24d43fec3da1bcaf17770158f9b854e
Sebastian <jslatorre@indracompany.com>
2024-06-17 17:04:15 -0500

    Creacion de login y otrosv1

--- src/pages/Car.tsx
+++ src/pages/Car.tsx
@@ -1,12 +1,6 @@
-import { useState } from 'react';
-import { Drawer, Typography, Box, IconButton, ListItem, List, ListItemText } from '@mui/material';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { useCart } from '../components/Car/CartContext';
 
 const Cart = () => {
-  const { cart, removeFromCart } = useCart();
-  const [open, setOpen] = useState(false);
+ 
 
   return (
     <>
